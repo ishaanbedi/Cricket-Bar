db841a81c5b5f9f53030080ea2044f138bfe1483
ishaanbedi <github@ishaanbedi.com>
2024-11-22 09:10:19 +0530

    Initial Commit

---
+++ Cricket Bar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cricket Bar
+//
+//  Created by Ishaan Bedi on 22/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cricket Bar/Cricket_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cricket_BarApp.swift
+//  Cricket Bar
+//
+//  Created by Ishaan Bedi on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Cricket_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
